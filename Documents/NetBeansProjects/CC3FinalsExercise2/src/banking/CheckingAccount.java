476cd91a865814788cafa71d6f27a9c329a6f1bf
knthlxs <109210257+knthlxs@users.noreply.github.com>
2023-05-09 19:27:16 +0800

    undo some comments

--- Documents/NetBeansProjects/CC3FinalsExercise2/src/banking/CheckingAccount.java
+++ Documents/NetBeansProjects/CC3FinalsExercise2/src/banking/CheckingAccount.java
@@ -1,7 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package banking;
 
 import javax.swing.JOptionPane;
 
+/**
+ *
+ * @author Kenth Alexis Osila
+ */
 public class CheckingAccount extends Account {
 
     @Override
